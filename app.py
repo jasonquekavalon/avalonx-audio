49eaabaac5a52468a16a39b69156c31daa2b92db
alexisloh <alexis.loh@avalonsolutions.com>
2019-04-03 15:16:10 +0200

    multiple languages

--- app.py
+++ app.py
@@ -25,24 +25,15 @@ def test_message(message):
     emit('my_response',
          {'data': message['data'], 'count': session['receive_count']})
 
-#@socketio.on('disconnect_request', namespace='/test')
-#def disconnect_request():
-#    session['receive_count'] = session.get('receive_count', 0) + 1
-#    emit('my_response',
-#         {'data': 'Disconnected!', 'count': session['receive_count']})
-# s    disconnect()
-
 @socketio.on('connect', namespace='/test')
 def test_connect():
     session['audio'] = []
-    emit('my_response', {'data': 'Connected', 'count': 0})
 
 @socketio.on('sample_rate', namespace='/test')
 def handle_my_sample_rate(sampleRate):
     session['sample_rate'] = sampleRate
     # send some message to front
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': "sampleRate : %s" % sampleRate, 'count': session['receive_count'] })
 
 @socketio.on('audio', namespace='/test')
 def handle_my_custom_event(audio):
@@ -51,8 +42,6 @@ def handle_my_custom_event(audio):
 
 @socketio.on('disconnect_request', namespace='/test')
 def test_disconnect():
-    print('disconnecting')
-    emit('my_response', {'data': 'Thinking...', 'count': 0})
     sample_rate = session['sample_rate']
     my_audio = np.array(session['audio'], np.float32)
     sindata = np.sin(my_audio)
@@ -65,32 +54,64 @@ def test_disconnect():
 
         """Transcribe the given audio file using an enhanced model."""
         # [START speech_transcribe_enhanced_model_beta]
+        from google.cloud import speech_v1p1beta1 as speech
         client = speech.SpeechClient()
 
-        first_lang = 'en-US'
-        second_lang = 'es'
-        third_lang = 'yue-Hant-HK'
-        fourth_lang = 'pl-PL'
+        first_lang = 'en-US'  #english
+        second_lang = 'es'  #spanish
+        third_lang = 'zh' #chinese
+        fourth_lang = 'pl-PL' #polish
+        fifth_lang = 'de-DE' #german
+        sixth_lang = 'sv-SE' #swedish
+        seventh_lang = 'nb-NO' #norwegian
+        eighth_lang = 'it-IT' #italian
 
-        print('transcribing')
+        language = {'en-us': 'english', 'es': 'spanish', 'cmn-hans-cn': 'chinese', 'pl-PL': 'polish', 'de-de': 'german', 'sv-SE': 'swedish', 'nb-no': 'norwegian', 'it-it': 'italian'}
+
         audio = speech.types.RecognitionAudio(content=f.getvalue())
         config = speech.types.RecognitionConfig(
             encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=44100,
+            sample_rate_hertz=48000,
             language_code=first_lang,
             alternative_language_codes=[second_lang,third_lang,fourth_lang])
-        print("running recognize")
-        response = client.recognize(config, audio, retry=None, timeout=60)
-        print("getting results")
+
+        response = client.recognize(config, audio)
+
         for i, result in enumerate(response.results):
             alternative = result.alternatives[0]
-            print('{}'.format(alternative.transcript))
-            print(response.results)
-            response = '{}'.format(alternative.transcript)
-        session['receive_count'] = session.get('receive_count', 0) + 1
-        emit('my_response', {'data': response, 'count': session['receive_count'] })
-        print(response)
+            confidence = float('{}'.format(alternative.confidence))
+            transcript = '{}'.format(alternative.transcript)
+            language_code =  str(result.language_code)
+            if confidence > 0.8:
+                confidence = int(confidence *100)
+                languages = language[language_code]
+                emit('my_response',
+                    {'data': 'I am %s percent confident that you are speaking %s' %(confidence,languages), 'count': session['receive_count']})
+            else : 
+                config = speech.types.RecognitionConfig(
+                encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=44100,
+                language_code=fifth_lang,
+                alternative_language_codes=[sixth_lang,seventh_lang,eighth_lang])
+
+                response = client.recognize(config, audio)
+
+                for i, result in enumerate(response.results):
+                    alternative = result.alternatives[0]
+                    confidence = float('{}'.format(alternative.confidence))
+                    transcript = '{}'.format(alternative.transcript)
+                    language_code =  str(result.language_code)
+                    if confidence > 0.8:
+                        confidence = int(confidence *100)
+                        languages = language[language_code]
+                        emit('my_response',
+                            {'data': 'I am %s percent confident that you are speaking %s' %(confidence,languages), 'count': session['receive_count']})
+                    else:
+                        emit('my_response',
+                            {'data': 'Sorry we could not recognise that language. Try speaking clearly', 'count': session['receive_count']})
+
     disconnect()
+
 
 @app.route('/speech_recognition', methods=['GET'])
 def game3():
